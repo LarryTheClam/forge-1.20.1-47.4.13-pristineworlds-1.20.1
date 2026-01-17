893b6147ffb30fee02f1e0631e968c87732d7a0b
wmder <wmderv@gmail.com>
2026-01-17 15:31:24 -0500

    1.1.3 Update - accidentally deregistered ModItems - Reregistered ModItems

--- src/main/java/net/pumpkin/pristineworlds/item/ModItems.java
+++ src/main/java/net/pumpkin/pristineworlds/item/ModItems.java
@@ -4,6 +4,7 @@ import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.HangingSignItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SignItem;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
